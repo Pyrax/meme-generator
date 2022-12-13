5d2990623d7528075e7c0812ef0d537875046163
Pyrax <piranhadev@gmail.com>
2022-12-13 21:14:10 +0100

    refactor: rename event handlers

--- src/components/editor/EditorActions.tsx
+++ src/components/editor/EditorActions.tsx
@@ -27,37 +27,37 @@ const EditorActions = ({ className, onSave, ...props }: EditorActionsProps) => {
     color: '#000000',
   };
 
-  const setTopText = () =>
+  const addTextTopHandler = () =>
     setText('top', {
       ...defaultText,
       position: 'top',
       offset: 12,
     } as TextElement);
 
-  const setBottomText = () =>
+  const addTextBottomHandler = () =>
     setText('bottom', {
       ...defaultText,
       position: 'bottom',
       offset: -12,
     } as TextElement);
 
-  const selectHandler = (selected: number) => {
+  const selectImageSaveHandler = (selected: number) => {
     const { extension, mimeType } = EXPORT_FILE_TYPES[selected];
     return onSave({ extension, mimeType });
   };
 
   return (
     <div className={classNames(className, 'px-4')} {...props}>
-      <Button onClick={() => setTopText()} disabled={!!textMap['top']}>
+      <Button onClick={addTextTopHandler} disabled={!!textMap['top']}>
         <FaFont /> Add top text
       </Button>
-      <Button onClick={() => setBottomText()} disabled={!!textMap['bottom']}>
+      <Button onClick={addTextBottomHandler} disabled={!!textMap['bottom']}>
         <FaFont /> Add bottom text
       </Button>
       <Button
         selectValues={EXPORT_FILE_TYPES.map((type) => type.extension)}
-        onDropdownSelect={selectHandler}
-        onClick={() => selectHandler(0)}
+        onDropdownSelect={selectImageSaveHandler}
+        onClick={() => selectImageSaveHandler(0)}
       >
         <FaDownload /> Save
       </Button>
